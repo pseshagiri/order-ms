c0d8f046fb7b28b9479a2341e85ade3527bd413b
pseshagiri <spichika@gmail.com>
2023-04-29 01:38:46 -0700

    added new api

--- src/main/java/com/seshagiri/controller/OrdersController.java
+++ src/main/java/com/seshagiri/controller/OrdersController.java
@@ -1,6 +1,7 @@
 package com.seshagiri.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,11 @@ public class OrdersController {
 	public String getAll() {
 		System.out.println("Called getALL Request ...");
 		return "Hai";
+	}
+	
+	@GetMapping("/{id}")
+	public String getById(@PathVariable("id") Integer id) {
+		System.out.println("Called getALL Request ...");
+		return "Hai-->" + id;
 	}
 }
